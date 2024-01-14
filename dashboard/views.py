71dfbffb0fd1fd3815b7140158ab76cd1fc8e90b
Om Aryan <aryan2003k@gmail.com>
2024-01-14 18:15:24 +0530

    implented tabular rep in dash

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,11 @@ from users.models import Room
 
 def dashboard(request):
     rooms = list(Room.objects.order_by('num'))
+    room_mat = []
+
+    for i in range(0,len(rooms),4):
+        room_mat.append(rooms[i:i+4])
     return render(request,'dashboard.html',{
-        'rooms':rooms
+        'rooms':rooms,
+        'room_mat':room_mat,
     })
